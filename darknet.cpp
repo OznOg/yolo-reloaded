5d5e027165b6002f2102ac47fa98b53790376198
OznOg <sebastien.gonzalve@aliceadsl.fr>
2017-09-16 19:44:54 +0200

    Open image and reshape it with openCV

--- darknet.cpp
+++ darknet.cpp
@@ -5,11 +5,15 @@
 #include <string>
 #include <vector>
 
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
 using namespace yolo;
 
 bool run_detect(const std::vector<std::string> &args) {
-    if (args.size() < 2) {
-	std::cerr << "usage: detect needs at least 2 parameters." << std::endl << std::endl;
+    if (args.size() < 3) {
+	std::cerr << "usage: detect needs at least 3 parameters." << std::endl << std::endl;
         return false;
     }
 
@@ -20,6 +24,37 @@ bool run_detect(const std::vector<std::string> &args) {
     if (!weightsFile)
         return false;
     net->loadWeights(weightsFile);
+
+    // FIXME not implemented yet... not sure what it is needed for...
+    // net->set_batch_network(1);
+
+    std::string file_name = args[2];
+    cv::Mat image = cv::imread(file_name);
+    if (image.empty()) {
+        std::cerr << "Could not load image '" << file_name << "'\n";
+        return false;
+    }
+
+    cv::Mat resized;
+    double resize_factor_x = ((double)net->_input_size.width) / image.cols;
+    double resize_factor_y = ((double)net->_input_size.height) / image.rows;
+
+    auto min = std::min(resize_factor_x, resize_factor_y);
+
+    // resize image (keeping ratio) so that it fits the network input
+    cv::resize(image, resized, cv::Size(0, 0), min, min);
+
+    cv::Mat letterbox(cv::Size(net->_input_size.width, net->_input_size.height), image.type());
+    size_t distance_to_top  = (letterbox.rows - resized.rows) / 2;
+    size_t distance_to_side = (letterbox.cols - resized.cols) / 2;
+
+    // resized image is copied into a network expected size, adding grey borders
+    copyMakeBorder(resized, letterbox, distance_to_top, distance_to_top,
+                   distance_to_side, distance_to_side,
+                   cv::BORDER_CONSTANT, cv::Scalar(0x7F, 0x7F, 0x7F));
+
+    cv::Mat floatInput;
+    letterbox.convertTo(floatInput, CV_32F);
 
     return true;
 }
