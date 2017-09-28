60c0161094313ed4646217b0f11843016bbd6809
OznOg <sebastien.gonzalve@aliceadsl.fr>
2017-09-28 20:49:25 +0200

    use standard opencv header
    on some distros, the compilation was failing

--- darknet.cpp
+++ darknet.cpp
@@ -5,9 +5,7 @@
 #include <string>
 #include <vector>
 
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/highgui.hpp>
+#include <opencv2/opencv.hpp>
 
 using namespace yolo;
 
