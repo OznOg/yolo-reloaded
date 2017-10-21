050e3aaae6467e8c41fbb23dff5f9de381f07b28
OznOg <sebastien.gonzalve@aliceadsl.fr>
2017-10-21 12:56:35 +0200

    eventually get the tile version of gmm work...

--- libclblas.cpp
+++ libclblas.cpp
@@ -37,20 +37,51 @@ class Setup {
 
        // kernel calculates for each element C=A+B
        std::string kernel_code { R"CLC(
-                               __kernel void gemm(const int M, const int N, const int K,
-                                                     const __global float* A,
-                                                     const __global float* B,
-                                                     __global float* C) {
+#define TS 16
+__kernel void gemm(const int M, const int N, const int K,
+                      const __global float* A,
+                      const __global float* B,
+                      __global float* C) {
+    // Thread identifiers
+    const int row = get_local_id(0); // Local row ID (max: TS)
+    const int col = get_local_id(1); // Local col ID (max: TS)
+    const int globalRow = TS*get_group_id(0) + row; // Row ID of C (0..M)
+    const int globalCol = TS*get_group_id(1) + col; // Col ID of C (0..N)
 
-                                   // Thread identifiers
-                                   const int globalRow = get_global_id(0); // Row ID of C (0..M)
-                                   const int globalCol = get_global_id(1); // Col ID of C (0..N)
+    // Local memory to fit a tile of TS*TS elements of A and B
+    __local float Asub[TS][TS];
+    __local float Bsub[TS][TS];
 
-                                   // Compute a single element (loop over K)
-                                   for (int k = 0; k < K; k++) {
-                                       C[globalCol + N * globalRow] += A[k + K * globalRow] * B[globalCol + N * k];
-                                   }
-                               }
+    // Initialise the accumulation register
+    float acc = 0.0f;
+    
+    // Loop over all tiles
+    const int numTiles = ceil(K / (float)TS);
+    for (int t = 0; t < numTiles; t++) {
+
+        // Load one tile of A and B into local memory
+        const int tiledRow = TS * t + row;
+        const int tiledCol = TS * t + col;
+        Asub[col][row] = tiledCol >= K || globalRow >= M ? 0 : A[tiledCol * M + globalRow];
+        Bsub[col][row] = globalCol >= N || tiledRow >= K ? 0 : B[globalCol * K + tiledRow];
+
+        // Synchronise to make sure the tile is loaded
+        barrier(CLK_LOCAL_MEM_FENCE);
+
+        // Perform the computation for a single tile
+        for (int k = 0; k < TS; k++) {
+            acc += Asub[k][row] * Bsub[col][k];
+        }
+
+        // Synchronise before loading the next tile
+        barrier(CLK_LOCAL_MEM_FENCE);
+    }
+
+if (globalCol >= N || globalRow >= M)
+return;
+    // Store the final result in C
+    C[globalCol*M + globalRow] = acc;
+}
                               )CLC"};
 
        sources.push_back({kernel_code.c_str(), kernel_code.length()});
@@ -119,13 +150,16 @@ void libclblas(float* A, int lda, float* B, int ldb, float* C, int ldc, float AL
     err = queue.enqueueWriteBuffer(bufC, CL_TRUE, 0, M*N*sizeof(*C), C, NULL, NULL);
 
     cl::Kernel gemmm = cl::Kernel(instance.getProgram(), "gemm");
-    gemmm.setArg(0, M);
-    gemmm.setArg(1, N);
+    gemmm.setArg(1, M);
+    gemmm.setArg(0, N);
     gemmm.setArg(2, K);
-    gemmm.setArg(3,bufA);
-    gemmm.setArg(4,bufB);
-    gemmm.setArg(5,bufC);
-    queue.enqueueNDRangeKernel(gemmm, cl::NullRange, cl::NDRange(M, N), cl::NullRange);
+    gemmm.setArg(4, bufA);
+    gemmm.setArg(3, bufB);
+    gemmm.setArg(5, bufC);
+
+#define TS 16
+    queue.enqueueNDRangeKernel(gemmm, cl::NullRange, cl::NDRange(N + TS - (N % TS), M + TS - (M % TS)), cl::NDRange(TS, TS));
+
     queue.finish();
 
     // Copy the output matrix C back to the CPU memory
